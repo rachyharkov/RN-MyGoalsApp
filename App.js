5d6ba7b5e37479d1d6a834614ff412a854bce8b0
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-11-19 20:07:59 +0700

    add image and visual tweak

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {
   FlatList,
   Button
 } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 import GoalItem from './components/GoalItem';
 import GoalInput from './components/GoalInput';
@@ -40,30 +41,33 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <Button title='Add New Goal' color='#5e0acc' onPress={startAddGoalHandler}/>
-      <GoalInput
-        visible={modalIsVisible} 
-        onAddGoal={addGoalHandler}
-        onCancel={endGoalHandler}
-      />
-      <View style={styles.goalsContainer}>
-        <FlatList 
-          data={courseGoals} 
-          renderItem={(itemData) => {
-            return (
-              <GoalItem 
-                textny={itemData.item.text} 
-                onDeleteItem={deleteGoalHandler} 
-                id={itemData.item.id}
-              />
-            )
-          }}
-          alwaysBounceVertical={false}
-          keyExtractor={(item, index) => item.id}
+    <>
+      <StatusBar style='light'/>
+      <View style={styles.appContainer}>
+        <Button title='Add New Goal' color='#a065ce' onPress={startAddGoalHandler}/>
+        <GoalInput
+          visible={modalIsVisible} 
+          onAddGoal={addGoalHandler}
+          onCancel={endGoalHandler}
         />
+        <View style={styles.goalsContainer}>
+          <FlatList 
+            data={courseGoals} 
+            renderItem={(itemData) => {
+              return (
+                <GoalItem 
+                  textny={itemData.item.text} 
+                  onDeleteItem={deleteGoalHandler} 
+                  id={itemData.item.id}
+                />
+              )
+            }}
+            alwaysBounceVertical={false}
+            keyExtractor={(item, index) => item.id}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -72,6 +76,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 50,
     paddingHorizontal: 16,
+    backgroundColor: '#311b6b'
   },
   goalsContainer: {
     flex: 7
--- components/GoalInput.js
+++ components/GoalInput.js
@@ -4,7 +4,8 @@ import {
     TextInput, 
     StyleSheet,
     View,
-    Modal
+    Modal,
+    Image
 } from 'react-native';
 
 function GoalInput(props) {
@@ -22,6 +23,7 @@ function GoalInput(props) {
     return (
         <Modal visible={props.visible} animationType="slide">
             <View style={styles.inputContainer}>
+                <Image style={styles.image} source={require('../assets/images/goal.png')}/>
                 <TextInput 
                     style={styles.textInput} 
                     placeholder="Ur Course goal"
@@ -30,13 +32,14 @@ function GoalInput(props) {
                 />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button}>
+                        <Button title="Cancel" onPress={props.onCancel} color="#f31282"/>
+                    </View>
+                    <View style={styles.button}>
                         <Button 
                             title="Add Goal"
                             onPress={addGoalHandler}
+                            color="#b180f0"
                         />
-                    </View>
-                    <View style={styles.button}>
-                        <Button title="Cancel" onPress={props.onCancel}/>
                     </View>
                 </View>
             </View>
@@ -51,14 +54,15 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        padding: 16
+        padding: 16,
+        backgroundColor: '#311b6b'
       },
       textInput: {
         borderWidth: 1,
-        borderColor: '#ccccccc',
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        color: '#120438',
+        borderRadius: 6,
         width: '100%',
         marginRight: 8,
         padding: 8
@@ -70,5 +74,10 @@ const styles = StyleSheet.create({
       button: {
         width: 100,
         marginHorizontal: 8
+      },
+      image: {
+        width: 100,
+        height: 100,
+        margin: 20
       }
 })
