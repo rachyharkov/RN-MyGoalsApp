346faf40ba217be5fb66e1f69f271d7d1b557c06
Rachmad Nur Hayat <rachmadnurhayat@gmail.com>
2022-11-19 17:40:48 +0700

    add feedback Visual Effect on  Android and iOS

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@ import { StyleSheet, View, Text, Pressable } from 'react-native'
 
 function GoalItem(props) {
     return(
-        <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-            <View style={styles.goalItem}>
+        <View style={styles.goalItem}>
+            <Pressable 
+                android_ripple={{color: '#fff'}} 
+                onPress={props.onDeleteItem.bind(this, props.id)}
+                style={({pressed}) => pressed && styles.pressedItem}
+            >
                 <Text style={styles.goalText}>{props.textny}</Text>
-            </View>
-        </Pressable>
+            </Pressable>
+        </View>
     )
 }
 
@@ -15,11 +19,16 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
+    padding: 0,
     borderRadius: 6,
     backgroundColor: "#3498db",
   },
+  pressedItem: {
+    // iOS: opacitynya bakal berubah
+    opacity: 0.5
+  },
   goalText: {
     color: "white",
+    padding: 8
   },
 });
